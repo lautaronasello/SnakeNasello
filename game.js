0f9f46a94af5f041df0b4aef1535b54d2264ace2
Lautaro Nasello <lautaronasello@gmail.com>
2021-05-04 11:29:02 -0300

    add movement

--- game.js
+++ game.js
@@ -24,5 +24,6 @@ function update() {
 }
 
 function draw() {
+  gameBoard.innerHTML = "";
   drawSnake(gameBoard);
 }
--- snake.js
+++ snake.js
@@ -1,20 +1,20 @@
 export const SNAKE_SPEED = 1;
-const snakebody = [
+const snakeBody = [
   { x: 10, y: 11 },
   { x: 11, y: 11 },
   { x: 12, y: 11 },
 ];
 
 export function update() {
-  for (let i = snakebody.lenght - 2; i >= 0; i--) {
-    snakebody[i + 1] = { ...snakebody[i] };
+  for (let i = snakeBody.lenght - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
   }
-  snakebody[0].x += 1;
-  snakebody[0].y += 0;
+  snakeBody[0].x += 1;
+  snakeBody[0].y += 0;
 }
 
 export function draw(gameBoard) {
-  snakebody.forEach((segment) => {
+  snakeBody.forEach((segment) => {
     const snakeElement = document.createElement("div"); //generates a div in the grid
     snakeElement.style.gridRowStart = segment.y; //gives the ubication of the row to start to draw
     snakeElement.style.gridColumnStart = segment.x; //gives the ubication of the column to start to draw
